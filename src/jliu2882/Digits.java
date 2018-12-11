ab411abdfa69bba3a6c6dd1dded0ff85dd879bc2
BT_1N3_32@bktech.local <BT_1N3_32@bktech.local>
2018-12-11 11:05:22 -0500

    Initial commit

---
+++ src/jliu2882/Digits.java
@@ -0,0 +1,28 @@
+package jliu2882;
+
+import java.util.ArrayList;
+
+public class Digits {
+    private ArrayList<Integer> digitList;
+
+    public Digits(int num){
+        this.digitList = new ArrayList<>();
+        String nums = String.valueOf(num);
+        for(int i =0 ; i < nums.length();i++){
+            digitList.add(Integer.parseInt(nums.substring(i,i+1)));
+        }
+    }
+
+    public boolean isStrictlyIncreasing(){
+        int temp  = 0;
+        for(int i= 0; i < digitList.size();i++){
+            if(digitList.get(i)>temp){
+                temp = digitList.get(i);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
